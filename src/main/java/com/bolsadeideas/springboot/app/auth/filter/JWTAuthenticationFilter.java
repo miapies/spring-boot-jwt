d55997ae35765c08c9a8e30cba48337cc147c082
Jimenez Jimenez <miapies78@gmail.com>
2018-07-20 13:58:31 +0200

    first commit

---
+++ src/main/java/com/bolsadeideas/springboot/app/auth/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,102 @@
+package com.bolsadeideas.springboot.app.auth.filter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.bolsadeideas.springboot.app.auth.service.JWTService;
+import com.bolsadeideas.springboot.app.auth.service.JWTServiceImpl;
+import com.bolsadeideas.springboot.app.models.entity.Usuario;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+	private JWTService jwtService;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTService jwtService) {
+		this.authenticationManager = authenticationManager;
+		setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/api/login", "POST"));
+
+		this.jwtService = jwtService;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+
+		String username = obtainUsername(request);
+		String password = obtainPassword(request);
+
+		if (username != null && password != null) {
+			logger.info("Username desde request parameter (form-data): " + username);
+			logger.info("Password desde request parameter (form-data): " + password);
+		} else {
+
+			Usuario user = null;
+			try {
+				user = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+				username = user.getUsername();
+				password = user.getPassword();
+
+				logger.info("Username desde request InputStream (raw): " + username);
+				logger.info("Password desde request InputStream (raw): " + password);
+			} catch (IOException e) {
+				logger.error("Error al obtener los datos del login raw", e);
+			}
+		}
+
+		username = username.trim();
+
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+		return authenticationManager.authenticate(authToken);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+
+		String token = jwtService.create(authResult);
+
+		response.addHeader(JWTServiceImpl.HEADER_STRING, JWTServiceImpl.TOKEN_PREFIX + token);
+		Map<String, Object> body = new HashMap<>();
+		body.put("token", token);
+		body.put("user", (User) authResult.getPrincipal());
+		body.put("mensaje", String.format("Hola %s, has iniciado sesión con éxito", authResult.getName()));
+
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+	}
+
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+
+		Map<String, Object> body = new HashMap<>();
+		body.put("mensaje", "Error de autenticación: username o password incorrectos!");
+		body.put("error", failed.getMessage());
+
+		response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+		response.setStatus(HttpStatus.UNAUTHORIZED.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+	}
+
+}
